37d53af66b618e37b7286463f36e854211b12a9e
Ale i <allleizq@gmail.com>
2022-08-16 22:44:38 -0700

    updated user routes and created setup for thought routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,6 +1,28 @@
 const router = require('express').Router();
 
+// GET to get all thoughts
+// GET to get a single thought by its _id
+// POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
+// PUT to update a thought by its _id
+// DELETE to remove a thought by its _id
 
+//  /api/thoughts
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought, 
+} = require('../../controllers/thoughtController');
+
+//gets thoughts and posts thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+router
+    .route('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought);
 
 
 
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,17 +1,29 @@
 const router = require('express').Router();
 
+// GET all users
+// GET a single user by its _id and populated thought and friend data
+// POST a new user:
+// PUT to update a user by its _id
+// DELETE to remove user by its _id
+
+//  /api/users
 const {
     getUsers,
     getSingleUser,
     createUser,
+    updateUser,
+    deleteUser, 
 } = require('../../controllers/userController');
 
 //this is combining the get and post route for user
-//  /api/users
 router.route('/').get(getUsers).post(createUser);
 
 
-router.route('/:userId').get(getSingleUser);
+router
+.route('/:userId')
+.get(getSingleUser)
+.put(updateUser)
+.delete(deleteUser);
 
 
 module.exports = router;
